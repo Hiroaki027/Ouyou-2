f40c3fc427bbe02d9fa2eb0aef4b4a9f3e508ad2
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-09-04 10:07:54 +0000

    Ouyou-8a

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,9 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-
+  has_many :user_rooms 
+  has_many :chats
+  has_many :rooms, through: :user_rooms　
 
   has_one_attached :profile_image
 
