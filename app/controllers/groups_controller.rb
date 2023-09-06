86f26281d613b7bcb7ef60a34f4af9d7f8d10303
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-09-06 11:36:40 +0000

    Ouyou-9c

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,28 +1,28 @@
 class GroupsController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
-  
+
   def index
     @book = Book.new
     @groups = Group.all
     @user = User.find(current_user.id)
   end
-  
+
   def show
     @book = Book.new
     @group = Group.find(params[:id])
     @user = User.find(params[:id])
   end
-  
+
   def join  #追記！
     @group = Group.find(params[:group_id])
     @group.users << current_user
     redirect_to  groups_path
   end
-  
+
   def new
     @group = Group.new
   end
-  
+
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
@@ -33,10 +33,10 @@ class GroupsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @group.update(group_params)
       redirect_to groups_path
@@ -44,16 +44,29 @@ class GroupsController < ApplicationController
       render "edit"
     end
   end
-  
+
   def destroy
     @group = Group.find(params[:id])
   #current_userは、@group.usersから消されるという記述。
     @group.users.delete(current_user)
     redirect_to groups_path
   end
-  
+
+  def new_mail
+    @group = Group.find(params[:group_id])
+  end
+
+  def send_mail
+    @group = Group.find(params[:group_id])
+    @grouo_name = @group.name
+    group_users = @group.users
+    @mail_title = params[:mail_title]
+    @mail_content = params[:mail_content]
+    ContactMailer.send_mail(@mail_title, @mail_content,group_users).deliver
+  end
+
   private
-  
+
   def group_params
     params.require(:group).permit(:name, :introduction, :group_image)
   end
