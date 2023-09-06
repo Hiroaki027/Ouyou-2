2fdf092d8e23ee1eef9c2681584165691aafc63d
HiroakiBaba <141119965+Hiroaki027@users.noreply.github.com>
2023-09-06 09:28:41 +0000

    Ouyou-8c修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
+    @group.users << current_user
     if @group.save
       redirect_to groups_path, method: :post
     else
@@ -42,6 +43,13 @@ class GroupsController < ApplicationController
     else
       render "edit"
     end
+  end
+  
+  def destroy
+    @group = Group.find(params[:id])
+  #current_userは、@group.usersから消されるという記述。
+    @group.users.delete(current_user)
+    redirect_to groups_path
   end
   
   private
